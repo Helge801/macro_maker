a345556b92052c69b0e96b73d9faac4b09321fb7
bryan lund <bryan.lund@rakuten.com>
2019-05-16 14:38:57 -0600

    gen syntax highlighting

--- react/src/components/codeeditor.js
+++ react/src/components/codeeditor.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { tokenize } from '../processors/tokenize.js';
 import '../stylesheets/codeeditor.css';
 
 class CodeEditor extends Component {
@@ -25,10 +26,42 @@ class CodeEditor extends Component {
 }
 
 function renderCode(text){
-  return `<pre className="code-pre" ref="di">${text.replace(/\b\w+/g,token => {
-    return `<span class="code-fun">${token}</span>`;
-  })}</pre>`
+  return (<pre className="code-pre">
+    {
+      tokenize(text).map(t =>{
+        if(t.match(/^\/.*\/$/)) return wrapSpan(t,"code-reg")
+        if(t.match(/^\".*\"$/)) return wrapSpan(t,"code-str")
+        if(t.match(/.+\($/))    return wrapSpan(t,"code-fun",true)
+        if(t.match(/^\d+$/))    return wrapSpan(t,"code-int")
+        if(t.match(/^\w+$/))    return wrapSpan(t,getWordClass(t))
+
+        return wrapSpan(t,"code-def")
+      })
+    }
+  </pre>)
 }
 
+function getWordClass(token){
+  switch(token){
+    case "id":
+    case "title":
+    case "description":
+    case "price":
+    case "price_old":
+    case "category":
+    case "url_product":
+    case "url_image":
+    case "url_imagebig":
+    case "currency":
+      return "code-res";
+    default:
+      return "code-err";
+  }
+}
+
+function wrapSpan(text, cls, pop){
+  if(pop) return <span className={cls}>{text.slice(0,text.length - 1)}<span className="code-def">{text[text.length -1]}</span></span>
+  return <span className={cls}>{text}</span>
+}
 
 export default CodeEditor;
---
+++ react/src/processors/tokenize.js
@@ -0,0 +1,121 @@
+export function tokenize(content){
+
+  var tokens = content.split('');
+  console.log(tokens)
+  tokens = groupEscapeable(tokens,'"',true);
+  console.log(tokens)
+  tokens = groupEscapeable(tokens,"/",true);
+  console.log(tokens)
+  tokens = groupSpace(tokens);
+  console.log(tokens);
+  tokens = groupTokens(tokens);
+  console.log(tokens)
+  return tokens
+
+}
+
+function groupEscapeable(tokens,token,escapeable){
+  var grouped = [], found, breakNext;
+
+  for(var i = 0;i < tokens.length; i++){
+    switch(tokens[i]){
+      case token:
+        if(found){
+          if(escapeable && i > 0 && tokens[i-1] === "\\"){
+            continue;
+          } else {
+            grouped.push(tokens.slice(found,i+1).join(''));
+            found = undefined;
+            token = getInversToken(token);
+          }
+        } else {
+          found = i;
+          token = getInversToken(token);
+        }
+        break;
+      default:
+        if(found) continue;
+        else grouped.push(tokens[i]);
+    }
+  }
+  return grouped;
+}
+
+function groupSpace(tokens){
+  var grouped = [],
+    space = ""
+
+  for( var i = 0; i < tokens.length; i++){
+    console.log("evaling", [tokens[i]])
+    if(tokens[i].match(/^[\s\r\n]$/)) space += tokens[i]
+    else {
+      console.log("didn't match")
+      if( space !== "" ) grouped.push(space)
+      space = ""
+      grouped.push(tokens[i])
+    }
+  }
+  return grouped;
+}
+
+function groupTokens(tokens){
+  var grouped = [], 
+    index,
+    token = "",
+    addToken = (t) => {
+      if(token) grouped.push(token);
+      token = "";
+      if(t) grouped.push(t);
+    };
+
+  for(var i = 0; i < tokens.length; i++){
+    if(tokens[i].match(/^[\s\n\r]+$/)){
+      addToken(tokens[i]);
+      continue;
+    }
+    switch(tokens[i]){
+
+      case ".":
+        addToken();
+        token += tokens[i];
+        break;
+
+      case ",":
+      case ")":
+      case "{":
+      case "}":
+      case "]":
+        addToken(tokens[i]);
+        break;
+
+      case "(":
+      case "[":
+        token += tokens[i];
+        addToken();
+        break;
+
+      default:
+        token += tokens[i];
+        if(i == tokens.length -1 ) addToken();
+
+    }
+  }
+  return grouped;
+}
+
+function getInversToken(token){
+  var inverter = {
+    "(":")",
+    ")":"(",
+    "[":"]",
+    "]":"[",
+    "{":"}",
+    "}":"{",
+    '"':'"',
+    "'":"'",
+    "`":"`",
+    "/":"/"
+  }
+  return inverter[token];
+}
+
